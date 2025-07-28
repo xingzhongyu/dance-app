942a03a84242bcaea956af4e03464e9eb9b3fec1
xzy <xzy12386@163.com>
2025-07-28 09:49:17 +0800

    minor

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://localhost:3001",
+    "http://sdu-112:3001",
     "http://43.153.52.246:81",
     "http://omicsml.ai:81"
 ]
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/dance',
+  reactStrictMode: true,
+  
+  // ↓↓↓ 添加这个关键配置 ↓↓↓
+  // assetPrefix 负责静态资源 (JS, CSS, images) 的 URL 前缀
+  // 在生产环境中, 我们把它设置为 basePath
+  assetPrefix: isProd ? '/dance' : undefined,
+
+  // 如果你使用了 output: 'export' (静态导出), 下面这个配置也可能需要
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
